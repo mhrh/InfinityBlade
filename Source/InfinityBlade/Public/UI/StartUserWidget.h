2bc9158965f843246ace61f7ce30a44eb82d2eeb
DESKTOP-6GS90J4\Raytine <lovesunx@163.com>
2018-06-27 18:56:25 +0800

    蓝图转C++

---
+++ Source/InfinityBlade/Public/UI/StartUserWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "StartUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITYBLADE_API UStartUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
